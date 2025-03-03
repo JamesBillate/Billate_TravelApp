95d121f0505cdffda25058e350fdc148e48129e1
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-03-03 15:53:07 +0800

    Fixed the frames

--- app/src/main/java/com/example/billate_travelapp/MainActivity.kt
+++ app/src/main/java/com/example/billate_travelapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.billate_travelapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnDestination: Button = findViewById(R.id.btnDestination)
+        val btnTravelTips: Button = findViewById(R.id.btnTips)
+        val btnGallery: Button = findViewById(R.id.btnGallery)
+        val btnContactUs: Button = findViewById(R.id.btnContacts)
+
+        btnDestination.setOnClickListener {
+           val intent = Intent(this, Destinations::class.java)
+            startActivity(intent)
+        }
+
+        btnTravelTips.setOnClickListener {
+            val intent = Intent(this, TravelTips::class.java)
+            startActivity(intent)
+        }
+
+        btnContactUs.setOnClickListener {
+            val intent = Intent(this, ContactUs::class.java)
+            startActivity(intent)
         }
     }
 }
