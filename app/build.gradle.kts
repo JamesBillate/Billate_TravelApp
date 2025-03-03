00cebe5ae692723776752e194ec40a91be17f783
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-03-03 15:26:30 +0800

    Added some frames

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.billate_travelapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.billate_travelapp"
